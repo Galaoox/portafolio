89794ff127384d363d1874d8b472cd056caf9919
Erick Vergara <erickandres30@gmail.com>
2025-08-07 07:44:32 -0500

    Agrega las integraciones de Partytown y Sitemap en la configuración de Astro. Se actualiza el archivo de configuración para incluir el nuevo dominio y se añaden las dependencias correspondientes en package.json y pnpm-lock.yaml. Se incorpora el script de Google Tag Manager en el componente Layout.astro para mejorar el seguimiento analítico.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown'
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.erick-avn.xyz',
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    // mdx(), 
+    sitemap(),
+    partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+    }),
+  ]
 });
